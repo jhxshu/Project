f8e843157172621809b1c23640c0d17efa9fcaf8
jhxshu <jhx3182948561@163.com>
2026-03-18 18:54:00 +0800

    通过beast实现http get请求

---
+++ mychat/gateserver/CServer.cpp
@@ -0,0 +1,27 @@
+﻿#include "CServer.h"
+#include "HTTPConnection.h"
+CServer::CServer(boost::asio::io_context& ioc, unsigned short& port) : _ioc(ioc), _acceptor(ioc, tcp::endpoint(tcp::v4(), port)), _socket(ioc) {
+
+
+}
+
+void CServer::Start() {
+	auto self = shared_from_this();  //如果直接用智能指针会出现两个智能指针管理一个对象
+	_acceptor.async_accept(_socket, [self](beast::error_code ec) {
+		try {
+			//出错就放弃连接， 继续监听其他连接
+			if (ec) {
+				self->Start();
+				return;
+			}
+			//创建新连接， 并创建HTTPConnection类管理连接
+			std::make_shared<HttpConnection>(std::move(self->_socket))->Start();
+
+			//继续监听
+			self->Start();
+		}
+		catch (std::exception& exp) {
+
+		}
+		});
+}
---
+++ mychat/gateserver/HTTPConnection.cpp
@@ -0,0 +1,66 @@
+﻿#include "HTTPConnection.h"
+#include "LogicSystem.h"
+HttpConnection::HttpConnection(tcp::socket socket) : _socket(std::move(socket)) {
+
+}
+
+void HttpConnection::Start() {
+	auto self = shared_from_this();
+	http::async_read(_socket, _buffer, _request, [self](beast::error_code ec, ::std::size_t bytes_transferred) {
+		try {
+			if (ec) {     //ec已经被重载bool()
+				std::cout << "http read err is " << ec.what() << std::endl;
+				return;
+			}
+
+			boost::ignore_unused(bytes_transferred);
+			self->HandleReq();
+			self->CheckDeadline();
+		}
+		catch (std::exception& exp) {
+			std::cout << "exception is " << exp.what() << std::endl;
+		}
+		});
+}
+
+void HttpConnection::HandleReq() {
+	//设置版本
+	_response.version(_request.version());
+	_response.keep_alive(false);
+	if (_request.method() == http::verb::get) {
+		bool success = LogicSystem::GetInstance()->HandleGet(_request.target(), shared_from_this());
+		if (!success) {
+			_response.result(http::status::not_found);
+			_response.set(http::field::content_type, "txt/plain");
+			beast::ostream(_response.body()) << "url not found\r\n";
+			WriteResponse();
+			return;
+		}
+		_response.result(http::status::ok);
+		_response.set(http::field::server, "GateServer");
+		WriteResponse();
+		return;
+	}
+}
+
+void HttpConnection::WriteResponse() {
+	auto self = shared_from_this();
+	_response.content_length(_response.body().size());
+	http::async_write(_socket, _response, [self](beast::error_code ec, ::std::size_t bytes_transferred){
+		self->_socket.shutdown(tcp::socket::shutdown_send, ec);
+		self->deadline_.cancel();
+		});
+}
+
+void HttpConnection::CheckDeadline() {
+	auto self = shared_from_this();
+	deadline_.async_wait([self](beast::error_code ec) {
+		if (!ec) {
+			self->_socket.close(ec);
+		}
+		});
+}
+//先关闭shutdown端，就取消回调；长时间没有回调触发，就定时器强制关闭（有time wait隐患）
+
+
+
